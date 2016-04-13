0a850217238eb8ca46e4262bcdeb2c6502348ee2
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2016-04-13 12:32:22 +0300

    Updated watches script with dashboard service

--- scripts/manage_watches.sh
+++ scripts/manage_watches.sh
@@ -28,24 +28,24 @@ if [ "${STATUS_ARRAY[$i]}" != "passing" ]; then
       echo -e "Jenkins job Deploy PROD Reporting is already running, Checking status in 10 seconds"
     fi
   fi
-  # if [ "${CHECK_ID_ARRAY[$i]}" = "service:repsrv:frontend-dev:8080" ]; then
-  #   CHECKJOB=$(curl http://172.29.101.40:8080/job/ReportingServices/job/DeployProdReporting/lastBuild/api/json | jq ".building")
-  #   if [ "${CHECKJOB}" = "false" ]; then
-  #     echo -e "Triggering Jenkins job Deploy PROD Reporting"
-  #     curl -X POST http://172.29.101.40:8080/job/ReportingServices/job/DeployProdReporting/build
-  #   else
-  #     echo -e "Jenkins job Deploy PROD Reporting is already running, Checking status in 10 seconds"
-  #   fi
-  # fi
-  # if [ "${CHECK_ID_ARRAY[$i]}" = "service:repsrv:frontend-prod:80" ]; then
-  #   CHECKJOB=$(curl http://172.29.101.40:8080/job/ReportingServices/job/DeployProdReporting/lastBuild/api/json | jq ".building")
-  #   if [ "${CHECKJOB}" = "false" ]; then
-  #     echo -e "Triggering Jenkins job Deploy PROD Reporting"
-  #     curl -X POST http://172.29.101.40:8080/job/ReportingServices/job/DeployProdReporting/build
-  #   else
-  #     echo -e "Jenkins job Deploy PROD Reporting is already running, Checking status in 10 seconds"
-  #   fi
-  # fi
+  if [ "${CHECK_ID_ARRAY[$i]}" = "service:repsrv:frontend-dev:8080" ]; then
+    CHECKJOB=$(curl http://172.29.100.189:8080/job/TestPlatformDeployment/job/TestDasboard/lastBuild/api/json | jq ".building")
+    if [ "${CHECKJOB}" = "false" ]; then
+      echo -e "Triggering Jenkins job Deploy DEV Dashboard"
+      curl -X POST "http://172.29.100.189:8080/job/TestPlatformDeployment/job/TestDasboard/buildWithParameters?Checkout_and_Test=no&Build_and_Push_Image=no&Deploy_Dev=no&Deploy_Prod=yes"
+    else
+      echo -e "Jenkins job Deploy DEV Dashboard is already running, Checking status in 10 seconds"
+    fi
+  fi
+  if [ "${CHECK_ID_ARRAY[$i]}" = "service:repsrv:frontend-prod:80" ]; then
+    CHECKJOB=$(curl http://172.29.100.189:8080/job/TestPlatformDeployment/job/TestDasboard/lastBuild/api/json | jq ".building")
+    if [ "${CHECKJOB}" = "false" ]; then
+      echo -e "Triggering Jenkins job Deploy PROD Dashboard"
+      curl -X POST "http://172.29.100.189:8080/job/TestPlatformDeployment/job/TestDasboard/buildWithParameters?Checkout_and_Test=no&Build_and_Push_Image=no&Deploy_Dev=no&Deploy_Prod=yes"
+    else
+      echo -e "Jenkins job Deploy PROD Dashboard is already running, Checking status in 10 seconds"
+    fi
+  fi
 break
 else
   echo -e "${NC}Status for ${CHECK_ID_ARRAY[$i]} is ${STATUS_ARRAY[$i]}"
